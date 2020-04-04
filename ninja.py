251d6b4deb79b481efbce2a1e03eadb7ba9f0627
neslihanayartepe <neslihanayartepe@hotmail.com>
2020-04-04 23:39:01 +0300

    firstcommit

---
+++ ninja.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+
+t = turtle.Turtle()
+
+t.circle(100)
+t.penup()
+t.setposition(-70,120)
+t.pendown()
+t.forward(30)
+t.penup()
+t.forward(40)
+t.pendown()
+t.forward(30)
+
+t.penup()
+t.setposition(-50,50)
+t.pendown()
+t.right(90)
+t.circle(30,-180)
